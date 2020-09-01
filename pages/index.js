933dda425c1f067776292100be49e55891c7889f
satoru-tanaka <sato252011@gmail.com>
2020-09-01 20:20:07 +0900

    add: base resource

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const page = () => {
+    const text = 'aaa'
+    return (
+        <>
+            <div>Welcome to next.js!</div>
+            <div>Welcome to {text}</div>
+            <div>Welcome to test</div>
+        </>
+    )
+}
+
+export default page
